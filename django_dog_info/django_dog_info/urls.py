040b14ed3c0adb9843c9b65c50bc60f431455c05
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-04-05 14:05:06 +0300

    Add debug-toolabar, dogs urls
    Включается веб-интерфейс для анализа запросов в режиме отладки.
    Включены маршруты приложения dogs

--- django_dog_info/django_dog_info/urls.py
+++ django_dog_info/django_dog_info/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django_dog_info import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('dogs.urls')),
 ]
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    urlpatterns += debug_toolbar_urls()
